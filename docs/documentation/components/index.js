49deb277dcbfbf13669ae9ffab59fb1523e2cf04
ludekarts <ludekarts@gmail.com>
2024-01-10 13:04:40 +0100

    Reimplemented Components + added Comets-docs

---
+++ docs/documentation/components/index.js
@@ -0,0 +1,68 @@
+import { html, createAppContext } from '../../../dist/utility-belt.modern.js';
+
+const { app, component } = createAppContext({ title: "My app" });
+
+const counterReducer = {
+  store: "counter",
+
+  state: 0,
+
+  actions: {
+    "INCREMENT": (state) => {
+      return state + 1;
+    },
+
+    "DECREMENT": (state) => {
+      return state - 1;
+    },
+  },
+};
+
+
+export const Counter = component(({ state: counter, dispatch }) => {
+  return html`
+  <div>
+    <span>${counter}</span>
+    <button onclick="${() => dispatch("INCREMENT")}">+</button>
+    <button onclick="${() => dispatch("DECREMENT")}">-</button>
+  </div>
+`;
+}, counterReducer);
+
+
+
+// -----------------------------------------------------
+
+const mainReducer = {
+
+  state: {
+    someGlobalValue: "hi!",
+  },
+
+  actions: {
+    "UPDATE_GLOBAL_VALUE": (state, value) => {
+      return { ...state, someGlobalValue: value };
+    },
+  },
+};
+
+
+const App = app(({ state, dispatch }) => {
+
+  const updateGloablValue = () => {
+    dispatch("UPDATE_GLOBAL_VALUE", "hello!");
+  };
+
+  return html`
+    <div>
+      <h1>${state.title}</h1>
+      <span>${state.someGlobalValue}</span>
+      <button onclick="${updateGloablValue}">Change Global Value</button>
+      ${Counter(state.counter)}
+    </div>
+  `;
+
+}, mainReducer);
+
+
+document.querySelector("#components-doc").appendChild(App);
--- src/utilities/component.js
+++ src/utilities/component.js
@@ -1,175 +1,294 @@
+import { uid } from "./general.js";
 import { dynamicElement } from "./template.js";
-import { isPromise } from "./general.js";
-
+import { createStore, createReducer } from "./minirdx";
 
 /*
-// USAGE:
 
-  import { component, html } from "@ludekarts/utility-belt";
+  [🧩 METHOD]:
+  createAppContext(initailState);
 
-  const MyComponent = component(props => {
+  [📓 DESC]:
+  Creates main App context with internal shared state.
 
-    const { createValue } = props;
+  [⚙️ USAGE]:
 
-    const [helloMessage, setHelloMessage] = createValue("helloMessage", "Hello for the first time");
+  import { createAppContext } from "@ludekarts/utility-belt";
+  . . .
 
-    const newHelloMessage = () => {
-      setHelloMessage(oldState => oldState.replace("first", "next"));
+  export const { app, component } = createAppContext({ title: "My app" });
+
+
+  -----------------------------------------------------
+
+  [🧩 METHOD]:
+  app(componentFn, reducerConfig);
+
+  [📓 DESC]:
+  Generates App element that can be rendered to the DOM.
+
+  [⚙️ USAGE]:
+
+  import { app } from "./myAppContext.js";
+  import { html } from "@ludekarts/utility-belt";
+
+  const reducer = {
+
+    state: {
+      someGlobalValue: "hi!",
+    },
+
+    actions: {
+      "UPDATE_GLOBAL_VALUE": (state, value) => {
+        return { ...state, someGlobalValue: value };
+      },
+    },
+  };
+
+  const App = app(({ state, dispatch }) => {
+
+    const updateGloablValue = () => {
+      dispatch("UPDATE_GLOBAL_VALUE", "hello!");
     };
 
     return html`
-      <button onclick="${newHelloMessage}">${helloMessage()}</button>
+      <div>
+        <h1>{state.title}</h1>
+        <span>${state.someGlobalValue}</span>
+        <button click="${updateGloablValue}">Change Global Value</button>
+      </div>
     `;
-  });
 
-  document.body.append(MyComponent());
+  }, reducer);
+
+  document.body.appendChild(App);
+
+
+  -----------------------------------------------------
+
+  [🧩 METHOD]:
+  component(componentFn, reducerConfig);
+
+  [📓 DESC]:
+  Cretaes component connected to App global state.
+
+  [⚙️ USAGE]:
+
+  import { component } from "./myAppContext.js";
+  import { html } from "@ludekarts/utility-belt";
+
+  const counterReducer = {
+      store: "counter",
+
+      state: 0,
+
+      actions: {
+        "INCREMENT": (state) => {
+          return state + 1;
+        },
+
+        "DECREMENT": (state) => {
+          return state + 1;
+        },
+      },
+    };
+  };
+
+  export const Counter = component(({ state, dispatch }) => {
+    return html`
+      <div>
+        <span>${state}</span>
+        <button onclick="${() => dispatch("INCREMENT")}">+</button>
+        <button onclick="${() => dispatch("DECREMENT")}">-</button>
+      </div>
+    `;
+  }, counterReducer);
+
+  . . .
+
+  in App component:
+  . . .
+
+  return html`
+    <div>
+      <h1>{state.title}</h1>
+      <span>${state.someGlobalValue}</span>
+      <button click="${updateGloablValue}">Change Global Value</button>
+      ${Counter(state.counter)}
+    </div>
+  `;
+
+
+  // -----------------------------------------------------
+
+  [💡 HINT]:
+  When creating components in componentFn() you have acces to couple utilitites (state, dispatch, effect, refs):
+
+  component(({state, dispatch, effect, refs }) => {
+    return html`...`;
+  }, reducerConfig);
+
+  - state:    Global state (or slice of global state) passed to the component function as an argument e.g.:
+              ```Counter(state.counter);```
+  - dispatch: Dispatch function that can be used to dispatch actions to the global state.
+  - effect:   A function that can be used to create effects that will be run when when any of the dependencies changes e.g.:
+              ```effect("myEffectUniqueName", () => {}, dep1, dep2, dep3);```
+  - refs:     A function that returns refs to DOM elements referenced in template e.g.:
+              ```refs().counter.innerHTML```
 
 */
 
-export function component(cmpFn) {
+export function createAppContext(initailState = {}) {
+  const initEvent = `@initialize_${uid("--short")}`;
+  const mainReducer = createReducer(initailState)
+    .on(initEvent, (state, initailState) => ({ ...state, ...initailState }))
+    .done();
+  const store = createStore(mainReducer);
+  const component = componentCreator(store);
 
-  let useRef;
-  let element;
-  let prevState;
-  let createValue;
+  const app = (componentFn, reducerConfig) => {
+    const { state, actions } = reducerConfig;
 
-  let onCreate;
-  let onCleanupCallback;
-  let onCreateCallback;
+    // Initialize state based on App state.
+    state && store.dispatch(initEvent, state);
 
-  function render(state, ...children) {
+    const AppComponent = component(componentFn, actions ? { actions } : undefined);
 
-    // If no state - only children.
-    if (state instanceof HTMLElement) {
-      children.unshift(state);
-      state = undefined;
-    }
+    // Force App re-render on every state change.
+    store.subscribe(state => AppComponent({ ...state }));
 
-    // Set internal API methods.
-    if (!createValue) {
-      createValue = createInternalStateManager(render, children);
-    }
-
-    if (!onCreate) {
-      onCreate = callback => !onCreateCallback && (onCreateCallback = callback);
-    }
-
-    // Create new dynamic element.
-    if (!element) {
-      // console.log("create");
-
-      useRef = (refName, callback) => element && callback(element.refs[refName]);
-      element = dynamicElement(cmpFn, { state, children, createValue, useRef, onCreate });
-      onCleanupCallback = onCreateCallback?.(element, element.refs, state);
-      prevState = state;
-    }
-
-    // Re-render with previouse State.
-    else if (state === undefined) {
-      // console.log("re-render");
-      element.update({ state: prevState, children, createValue, useRef, onCreate });
-    }
-
-    // Update only when State changes.
-    else if (prevState !== state) {
-      // console.log("update");
-      element.update({ state, children, createValue, useRef, onCreate });
-      prevState = state;
-    }
-
-    return element;
+    return AppComponent(store.getState());
   }
 
-  // Cleanup.
-
-  render.cleanup = function () {
-    onCleanupCallback?.();
-
-    element.remove();
-    element.refs = null;
-    element.update = null;
-    element = null;
-
-    prevState = null;
-    createValue = null;
-
-    onCreate = null;
-    onCreateCallback = null;
-    onCleanupCallback = null;
-
-    useRef = null;
-  }
-
-  return render;
+  return Object.freeze({
+    app,
+    component,
+  });
 }
 
 
-// ---- Internal State Manager ----------------
+/*
 
-function createInternalStateManager(render, children) {
-  let states = new Map();
-  let values = [];
+  [DOCS 📋]:
 
-  return (key, initValue) => {
-
-    // On re-render.
-    if (states.has(key)) {
-      return states.get(key);
+  const reducerConfig = {
+    store?: "selector.of.value.in.stroe",
+    state?: "initail state",
+    actions: {
+      "ACTION_NAME": (oldState, actionPayloaa) => newState,
     }
+  }
 
-    // On first render.
-    else {
+*/
 
-      if (typeof initValue === "function") {
-        const unwrapedValue = initValue();
 
-        // Resolve & store promise value.
-        if (isPromise(unwrapedValue)) {
-          values.push(undefined);
+function componentCreator(store) {
+  let dispatch = store.dispatch;
 
-          let index = values.length - 1;
+  return function component(componentFn, reducerConfig) {
 
-          unwrapedValue
-            .then(value => {
-              values[index] = value;
-              render(undefined, ...children);
-            })
-            .catch(error => {
-              values[index] = error;
-              render(undefined, ...children);
-            });
-        }
+    let element;
+    let prevState;
 
-        // Store sync value.
-        else {
-          values.push(unwrapedValue);
-        }
+    let refs;
+    let effect;
+    let effects = new Map();
+
+    if (reducerConfig) {
+      if (reducerConfig.hasOwnProperty("actions")) {
+        store.extendReducer(createReducerFromConfig(reducerConfig), reducerConfig.store);
       }
 
       else {
-        values.push(initValue);
+        throw new Error("ComponentError: reducerConfig should have actions key");
+      }
+    }
+
+    function render(state) {
+
+      // Create new dynamic element.
+      if (!element) {
+        refs = () => element?.d?.refs;
+        effect = cerateEffectsStore(effects, element);
+        element = dynamicElement(componentFn, state, { dispatch, effect, refs });
+        runEffects(effects);
+        prevState = state;
       }
 
-      let index = values.length - 1;
-      let getset = [
+      // Return element with no re-render.
+      else if (state === null) {
+        return element;
+      }
 
-        function getValue(defaultValue) {
-          return values[index] === undefined && defaultValue !== undefined
-            ? defaultValue
-            : values[index];
-        },
+      // Re-render with previouse State.
+      else if (state === undefined) {
+        element.d.update(prevState, { dispatch, effect, refs });
+      }
 
-        function setValue(value) {
-          values[index] = typeof value === "function" ? value(values[index]) : value;
-          render(undefined, ...children);
-          return values[index];
-        }
+      // Update only when State changes.
+      else if (prevState !== state) {
+        element.d.update(state, { dispatch, effect, refs });
+        prevState = state;
+      }
 
-      ];
-
-      states.set(key, getset);
-      return getset;
+      return element;
     }
-  };
+
+    render.destroy = () => {
+      effects.forEach(({ teardown }) => teardown?.());
+      element.remove();
+      refs = undefined;
+      effect = undefined;
+      effect = undefined;
+      element = undefined;
+      dispatch = undefined;
+      prevState = undefined;
+    };
+
+    return render;
+  }
+}
+
+
+function cerateEffectsStore(effects) {
+
+  return (name, callback, ...deps) => {
+    // Add new effect. This will only register effect to be called (with runEffects()) when the element is created.
+    if (!effects.has(name)) {
+      effects.set(name, { deps, callback });
+    }
+
+    // Update effects (on re-render).
+    else {
+      const effect = effects.get(name);
+      if (mismatchDeps(deps, effect.deps)) {
+        effect.teardown?.();
+        effects.set(name, { deps });
+        const teardown = callback();
+        effects.set(name, { deps, teardown });
+      }
+    }
+  }
+}
+
+// Runs effects after element is created.
+function runEffects(effects) {
+  for (const [name, { callback, deps }] of effects.entries()) {
+    const teardown = callback();
+    effects.set(name, { teardown, deps });
+  }
+}
+
+// Compare dependency arrays.
+function mismatchDeps(depsA, depsB) {
+  return depsA.length === depsB.length ? depsA.some((d, i) => d !== depsB[i]) : true;
+}
+
+// Heler to turn reducer-config into proper reducer function.
+function createReducerFromConfig(config) {
+  const reducer = Object.keys(config.actions).reduce((acc, action) => {
+    return acc.on(action, config.actions[action]);
+  }, createReducer(config.state));
+
+  return reducer.done();
 }
 
