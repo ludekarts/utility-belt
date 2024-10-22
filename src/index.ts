d9484ff8c9b9e30818b15d1b01e872e2d071ee5d
ludekarts <ludekarts@gmail.com>
2024-10-22 09:51:21 +0200

    Move Clipboard helper

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./utils/query";
 export * from "./utils/arrays";
 export * from "./utils/strings";
 export * from "./utils/componens";
+export * from "./utils/clipboard";
---
+++ src/utils/clipboard.ts
@@ -0,0 +1,29 @@
+/**
+ * Allows for copying text to clipboard.
+ *
+ * @example
+ * copyText("Hello, World!", document.querySelector("#someElement"));
+ */
+export default function copyText(value: string, rootElement?: HTMLElement) {
+  const container = (document.getElementById("_clipContainer_") ||
+    document.createElement("textarea")) as HTMLTextAreaElement;
+  const root = rootElement || document.body;
+
+  if (!container.id) {
+    container.id = "_clipContainer_";
+    container.style.opacity = "0";
+    container.style.position = "fixed";
+    container.style.pointerEvents = "none";
+    root.appendChild(container);
+  }
+
+  try {
+    container.value = value;
+    container.select();
+    document.execCommand("copy");
+    return value;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
